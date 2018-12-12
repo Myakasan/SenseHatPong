86bb7ec58e727cde075633c3e19268a197bb6ca8
Etienne Mya K <egendron6@icloud.com>
2018-12-12 09:51:29 +0100

    Ajout balle + direction

--- script.py
+++ script.py
@@ -1,3 +1,14 @@
+#Initialisation du SenseHat
 from sense_hat import SenseHat
-
 sense = SenseHat()
+#Nettoyage de l'ecran
+sense.clear(0,0,0)
+#Initialisation du pixel de départ
+x=4
+y=4
+sense.set_pixel(x,y,255,255,255)
+#Si le SenseHat est bien initialisé alors je récupère les informations
+#du joystick et les print en console.
+while True:
+    for event in sense.stick.get_events():
+        print(event.direction, event.action)
